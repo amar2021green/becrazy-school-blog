f2643deae9834f683f9fc374a85b847be6fc487e
amar2021green <R17112021s>
2019-12-10 23:04:48 +0900

    Update web.php
    master用(管理画面)のルーティング実装

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,26 @@
 */
 
 Route::get('/', function () {
-    return view('top');
+  return view('top');
 });
-Route::get('/news', function () {
-    return view('news');
-});
+
+
+
+
+//URLがmaster/〜 のグルーピング。
+//masterは管理画面になるためログインや一般ユーザー向けのフロント画面は別のURLとコントローラを使うこと
+Route::prefix('master')->group(function () {
+
+  //
+  //Route::get('users', 'MasterController@users');
+  //Route::get('addUser', 'MasterController@showAddUser');
+  //Route::post('addUser', 'MasterController@addUser');
+
+  //記事管理（投稿、削除、更新）
+  Route::get('blogList', 'MasterController@blogList');
+  Route::get('addPost', 'MasterController@showAddPost');
+  Route::post('addPost', 'MasterController@addPost');
+  Route::get('updatePost', 'MasterController@showUpdatePost');
+  Route::post('updatePost', 'MasterController@updatePost');
+
+})
