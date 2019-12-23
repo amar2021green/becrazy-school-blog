7963c2d2d2325befbdf3de2df9bf9f403eb8d92f
amar2021green <R17112021s>
2019-12-23 21:21:57 +0900

    Update MasterController.php
    登録時に、必須カラムを指定してインスタンスにリクエスト時に入力したvalueを代入している構文を追加

--- app/Http/Controllers/MasterController.php
+++ app/Http/Controllers/MasterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 
 class MasterController extends Controller {
 
@@ -19,32 +20,49 @@ class MasterController extends Controller {
     * Post登録処理
     * @param Request $request Postリクエスト
     */
-    public function addTodolists(Request $request){
+    public function ShowPost(Request $request){
 
-      if($request->filled('title')){
-        $title = $request->input('title');
-        //リクエストされたtitleデータを取得して$titleに代入(表示ではない)
-      }
-      if($request->filled('memo')){
-        $memo = $request->input('memo');
-        //リクエストされたmemoデータを取得して$memoに代入(表示ではない)
-      }
 
-      if($request->filled('completed_at')){
-        $completed_at = $request->input('completed_at');
-        //リクエストされたcompleted_atデータを取得して$completed_atに代入(表示ではない)
-      }
+        if($request->filled('title')){
+          $request->title;
+          //リクエストされたtitleデータを取得して$titleに代入(表示ではない)
+        }
+          if($request->filled('content')){
+            $request->content;
+          //リクエストされたcontentデータを取得して$contentに代入(表示ではない)
+          }
 
-      $新規データ = new TodoList();
+
+
+
+
+      $新規blog = new Post();
       //新しいデータを登録する宣言みたいなもの
-      //new TodoList();でtodolistsテーブルの１レコード(id,title...)が作成されるイメージ
+      //new Post();でpostsテーブルの１レコード(id,title...)が作成されるイメージ
 
-      $新規データ->title = $title;
-      $新規データ->memo = $memo;
-      $新規データ->save();
-      //フォームから入力されたデータ(title,memo)が1つのレコードとして新規データに追加(save)された
-      return redirect('/todolists');
-    }
+      $新規blog->user_id = 1;
+      //postでサーバに送る際にuser_idに１を指定してリクエストしている
+      //※今回の登録時にはuser_idは必須
+
+      $新規blog->title = $request->title;
+      $新規blog->content = $request->content;
+      $新規blog->status = $request->status;
+      $新規blog->slug = $request->slug;
+      //上記は'新規blog'の'title','content'等のインスタンスに
+      //リクエスト時に入力されたtitle,content等のvalueが代入される
+
+
+      $新規blog->type = 'article';
+      $新規blog->mime_type = 'text/html';
+      //上記は'新規blog'の'type','mime_type'インスタンスに指定のvalueを入れている
+      //※mime_typeに入れた値は記事なのでhtmlを指定している
+      
+      $新規blog->save();
+      //フォームから入力されたデータ(title,content)等が
+      //1つのレコードとして新規データに追加(save)された
+      return redirect('/master/bloglist');
+      }
+
 
 
     public function editdone(Request $request){
@@ -108,6 +126,5 @@ class MasterController extends Controller {
 //$todo->title = $request->titleで未完了リストで編集したいidをリクエストしたtitleが入る
 //$todo->save();で更新したtodoがsaveされる（更新）
 //その後リターンで未完了リストにリダイレクトする
-
 
 }
