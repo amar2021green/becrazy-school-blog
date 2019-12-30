1cf2dd1a34b1fb557a50222e141426651111bb1b
amar2021green <R17112021s>
2019-12-30 10:07:00 +0900

    Update MasterController.php
    editcontntメソッドの中身編集
    （バリデートがフォーム入力値以外も指定していたため、更新タップ時にバリデートがかかって更新画面のままリダイレクトされていた）

--- app/Http/Controllers/MasterController.php
+++ app/Http/Controllers/MasterController.php
@@ -20,7 +20,7 @@ class MasterController extends Controller {
     * Post登録処理
     * @param Request $request Postリクエスト
     */
-    public function ShowPost(Request $request){
+    public function post(Request $request){
 
 
         if($request->filled('title')){
@@ -32,99 +32,80 @@ class MasterController extends Controller {
           //リクエストされたcontentデータを取得して$contentに代入(表示ではない)
           }
 
-
-
-
-
       $新規blog = new Post();
       //新しいデータを登録する宣言みたいなもの
       //new Post();でpostsテーブルの１レコード(id,title...)が作成されるイメージ
 
       $新規blog->user_id = 1;
       //postでサーバに送る際にuser_idに１を指定してリクエストしている
-      //※今回の登録時にはuser_idは必須
+      //※今回の登録時にはuser_idの指定は必須
 
       $新規blog->title = $request->title;
       $新規blog->content = $request->content;
       $新規blog->status = $request->status;
       $新規blog->slug = $request->slug;
       //上記は'新規blog'の'title','content'等のインスタンスに
-      //リクエスト時に入力されたtitle,content等のvalueが代入される
+      //リクエスト時に入力されたtitle,content等のvalueを代入した
 
 
       $新規blog->type = 'article';
       $新規blog->mime_type = 'text/html';
       //上記は'新規blog'の'type','mime_type'インスタンスに指定のvalueを入れている
-      //※mime_typeに入れた値は記事なのでhtmlを指定している
-      
+      //※mime_typeに入れた値は、htmlを指定している
+
       $新規blog->save();
       //フォームから入力されたデータ(title,content)等が
       //1つのレコードとして新規データに追加(save)された
       return redirect('/master/bloglist');
       }
-
-
-
-    public function editdone(Request $request){
-
-      $request->validate([
-        'id' => 'required'
-      ]);
-    //完了の実行なのでリクエストするのはidだけでいい。
-
-      //now()とはカーボンクラスから内部的に関数を実行している(カーボンクラスはuseしていない)
-      $post = TodoList::find($request->id);
-      $post->completed_at = now();
-      $post->save();
-      return redirect('/doneTodolists');
-    }
-
-    public function doneshow(Request $request){
-      $done = TodoList::whereNotNull('completed_at')->get();
-      return view('post.doneTodolists',array('done' => $done));
-
-    }
-
-    public function delete(Request $request){
-      // バリデーション
-      $validatedData = $request->validate([
-        'id' => 'required'
-      ]);
-
-      TodoList::destroy($request->id);
-      return redirect('todolists');
-    }
-
+      //リダイレクトでURLパスを直接指定
 
 
 //編集フォームをviewする
-    public function editshow(Request $request){
-      $edittodo = TodoList::find($request->editid);
-      return view('post.editTodolists',['todo' => $edittodo]);
+    public function editcontent($id){
+      $edit = Post::find($id);
+      return view('post.editblog',['編集blog' => $edit]);
     }
-    //リクエストされたeditidから対象レコードを取得して、$edittodoに代入
-    //リターンviewでポスト配下のeditTodolistsファイルを表示させる
+    //リクエストされたeditidから対象レコードを取得して、$editに代入
+    //リターンviewでpost配下のeditblogファイルを表示させる
 
 
 
 //編集フォームで入力したものをデータ更新する
-    public function update(Request $request){
+    public function updatecontent(Request $request){
       $request->validate([
         'id' => 'required',
         'title' => 'required',
-        'memo' => 'required'
-      ]);
+        'content' => 'required',
+        'status' => 'required'
+      ]);//フォームの入力チェック
 
-
-      $todo = TodoList::find($request->id);
-      $todo->title = $request->title;
-      $todo->memo = $request->memo;
-      $todo->save();
-      return redirect('/todolists');
+      $編集blog = Post::find($request->id);
+      $編集blog->title = $request->title;
+      $編集blog->content = $request->content;
+      $編集blog->status = $request->status;
+      $編集blog->save();
+      return redirect('/master/bloglist');
     }
-//更新動作は$todoにリクエストされたidをもとにレコードインスタンスを代入
-//$todo->title = $request->titleで未完了リストで編集したいidをリクエストしたtitleが入る
-//$todo->save();で更新したtodoがsaveされる（更新）
-//その後リターンで未完了リストにリダイレクトする
+//更新動作は$編集blogにリクエストされたidをもとにレコードインスタンスを代入
+//$編集blog->title = $request->titleでblogリストで、
+//編集したい記事のidとリクエストしたtitleが紐付けられる
+
+//$編集blog->save();で更新した編集blogがsaveされる（更新）
+//その後リターンでblogリストにリダイレクトする
+
+
+
+public function delete(Request $request){
+  // バリデーション
+  $validatedData = $request->validate([
+    'id' => 'required'
+  ]);
+
+  Post::destroy($request->id);
+  return redirect('/master/bloglist');
+}
+
+
 
 }
