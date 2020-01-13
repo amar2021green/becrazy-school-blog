d4c25f7c4e3948bc5504f33d2f1e3eabaacf1b15
amar2021green <R17112021s>
2020-01-13 10:21:21 +0900

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    //
+    use SoftDeletes;
+    //ソフトデリートを使う宣言；2020/01/07
 }
