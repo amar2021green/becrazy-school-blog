e68b5afd2a0e5a2c3391a3ada79fa56961de4654
amar2021green <R17112021s>
2020-01-15 21:40:03 +0900

    Update MasterRegisterController.php
    registerできるユーザーはログイン済みのユーザーでなければできない仕様なのでregisterフォームに行く前にログイン済みかチェックしてくれるconstructを仕込んだ

--- app/Http/Controllers/Auth/MasterRegisterController.php
+++ app/Http/Controllers/Auth/MasterRegisterController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers\Auth;
 class MasterRegisterController extends RegisterController
 {
 
+  public function __construct() {
+     $this->middleware('auth');
+  }
+
   public function showRegistrationForm(){
     return view('user.register');
   }
+  //registerできるユーザーはログイン済みのユーザーでなければできない仕様なのでregisterフォームに行く前にログイン済みかチェックしてくれるconstructを仕込んだ
 
     /**
      * Where to redirect users after login.
