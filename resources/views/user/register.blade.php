6945bb4a70d4fee94297b8d52591ded7f413f454
amar2021green <R17112021s>
2020-01-15 21:42:14 +0900

    Update register.blade.php
    action="{{ route('user.register') }}">というルーティングに記載の->name('name')という実装を変更したので、
    URLを直接指定するaction="/master/register">に変更

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -9,7 +9,7 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('user.register') }}">
+                    <form method="POST" action="/master/register">
                         @csrf
 
                         <div class="form-group row">
