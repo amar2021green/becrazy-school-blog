662fd8d521c8d7a47d7c89447f19c4ad7bc442b5
amar2021green <R17112021s>
2020-01-16 22:38:30 +0900

    Update formblog.blade.php
    ・Statusをプルダウン式に設定
    ・Tagカテゴリを追加

--- resources/views/post/formblog.blade.php
+++ resources/views/post/formblog.blade.php
@@ -13,13 +13,27 @@
     <dt>Content</dt>
     <dd><textarea name="content" required>{{ old('content') }}</textarea></dd>
 
-    <dt>Status</dt>
-    <dd><input type="text" name="status" required value="{{ old('status') }}"></dd>
-    //公開範囲の指定
+    <dt>Tag</dt>
+    <dd>
+      <select id="Tag1" name="Tag1" size="5" multiple>
+        <option value="1">アジア</option>
+        <option value="2">北米</option>
+        <option value="3">欧米</option>
+        <option value="4">南米</option>
+        <option value="5">アフリカ</option>
+      </select>
 
-    <dt>Slug</dt>
+    <dt>Status(公開/非公開)</dt>
+    <dd><select name="publish">
+      <option required value="{{ old('publish') }}">Open</option>
+      <option>draft</option>
+        </select>
+      </dd>
+
+
+    <dt>Slug(URL)</dt>
     <dd><input type="text" name="slug" required value="{{ old('slug') }}"></dd>
-    //記事のURL指定
+
 
 
     </dl>
