f2c025dadac0049d1cda5ce3452c4baa8480b24f
amar2021green <R17112021s>
2020-01-19 15:15:32 +0900

    Update formblog.blade.php

--- resources/views/post/formblog.blade.php
+++ resources/views/post/formblog.blade.php
@@ -13,16 +13,6 @@
     <dt>Content</dt>
     <dd><textarea name="content" required>{{ old('content') }}</textarea></dd>
 
-    <dt>Tag</dt>
-    <dd>
-      <select id="Tag1" name="Tag1" size="5" multiple>
-        <option value="1">アジア</option>
-        <option value="2">北米</option>
-        <option value="3">欧米</option>
-        <option value="4">南米</option>
-        <option value="5">アフリカ</option>
-      </select>
-
     <dt>Status(公開/非公開)</dt>
     <dd><select name="publish">
       <option required value="{{ old('publish') }}">Open</option>
