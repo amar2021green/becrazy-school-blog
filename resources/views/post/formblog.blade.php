e84d40fcefb80007ff197c3f043b372cd32b918a
amar2021green <R17112021s>
2020-01-27 21:43:50 +0900

    記事投稿フォームでTaxonomyに登録されたTagとCategoryを選択できるように実装
    TagとCategoryのそれぞれのフォームにTaxonomyに登録された下記2つのどちらかを指定するように実装
    ・type=tag
    ・type＝category

--- resources/views/post/formblog.blade.php
+++ resources/views/post/formblog.blade.php
@@ -24,7 +24,37 @@
     <dt>Slug(URL)</dt>
     <dd><input type="text" name="slug" required value="{{ old('slug') }}"></dd>
 
+    <dt>Tag</dt>
+    <dd><select name="tag">
+      @foreach($allType as $tag)
+        @if($tag->type == 'tag')
+          <option value="{{($tag->id)}}">
+            {{$tag->name}}
+          </option>
 
+        @endif
+      @endforeach
+        </select>
+      </dd>
+
+      {{--👆フォームにTaxonomyテーブルに登録されたTagデータを入れたい👆--}}
+      {{--Taxonomyテーブルのコレクションが入った$allTypeをforeachで回すため$allType as $tag（Taxonomyのレコードが増えるたびに<option value="taxonomy">国名</option>と書かなきゃいけないのを防ぐため、@foreach を使う）--}}
+      {{--このままだとtypeが全て同じものをDBからとってくるので--}}
+      {{--@if($tag->type == 'tag') として $tagのtypeがtagのもの指定する--}}
+      {{--<option value="{{($tag->id)}}">としてidをサーバーに送る--}}
+      {{--最後のセレクトに表示される名前を{{$tag->name}}としてtypeがtagのnameを表示する--}}
+
+      <dt>Category<dt>
+        <dd><select name="category">
+          @foreach($allType as $category)
+            @if($category->type == 'category')
+              <option value="$category->id">
+                {{$category->name}}
+              </option>
+            @endif
+          @endforeach
+        </select>
+      </dd>
 
     </dl>
 
