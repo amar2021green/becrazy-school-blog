0b0c550b34f675459221c72ca0872c6a82ab9dcd
amar2021green <R17112021s>
2020-02-04 07:09:37 +0900

    Update japan.blade.php

--- resources/views/ordinary/japan.blade.php
+++ resources/views/ordinary/japan.blade.php
@@ -4,14 +4,14 @@
 <div class="p-3 mb-2 bg-info text-white">
 
   <div class="flex-center position-ref">
-    <h1><b>Japan</b></h1>
+    <h1><b>{{$kizi1->title}}</b></h1>
     <ul>
 
-    <li>
-    {{$kizi1->title}}
+
+
     {{$kizi1->content}}
 
-    </li>
+    
     <br>
 
       @csrf
