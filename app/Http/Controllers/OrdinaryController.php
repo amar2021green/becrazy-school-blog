3e2dc5744cf9e187b0d465140fc430afc804046d
amar2021green <R17112021s>
2020-02-04 07:09:31 +0900

    タグに紐付いた記事一覧の各記事へ飛べるようにリンク設定
    viewの指定はordinaryフォルダのtokyoファイル

--- app/Http/Controllers/OrdinaryController.php
+++ app/Http/Controllers/OrdinaryController.php
@@ -16,6 +16,7 @@ class OrdinaryController extends Controller {
       return view('ordinary.list',array('all' => $all));
   }
 
+
   public function ShowContents($slug)
   {
     $kizi1 = Post::where('slug',$slug)->first();
@@ -23,16 +24,10 @@ class OrdinaryController extends Controller {
 		return view('ordinary.japan',array('kizi1' => $kizi1));
 	}
 
-
-  public function ShowTag($slug)
-  {//typeがTagでなおかつslugにあてはまるwhere文を入れる
-    $tag1 = Taxonomy::where([
-      ['type','tag'],
-      ['slug',$slug]])->first();
-    //$tag1にはTaxonomyモデルのインスタンスが入る
-		return view('ordinary.tags',array('tag1' => $tag1));
-	}
-
+  public function TagPost($slug){
+    $TagPost = Post::where('slug',$slug)->first();
+      return view('ordinary.tokyo',array('TagPost' => $TagPost));
+  }
 
   public function ShowCategory($slug)
   {//typeがcategoryでなおかつslugにあてはまるwhere文を入れる
