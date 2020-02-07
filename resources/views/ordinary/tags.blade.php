daefa5a9fcd7b74dad27d5598060262d6df531d0
amar2021green <R17112021s>
2020-02-07 21:50:16 +0900

    Tagsの背景画像追加

--- resources/views/ordinary/tags.blade.php
+++ resources/views/ordinary/tags.blade.php
@@ -1,8 +1,8 @@
 @extends('layout.flame')
 @section('title','Tag')
 @section('body')
-<div class="p-3 mb-2 bg-info text-white">
-
+<div class="p-3 haikei">
+  
   <div class="flex-center position-ref">
     <h1><b>Tag</b></h1>
     <ul>
