f65146dd7045b4c529fdd08369fa3f5b1c7bb706
amar2021green <R17112021s>
2020-02-07 22:42:16 +0900

    記事のflameを一般viewのものに適用

--- resources/views/ordinary/tokyo.blade.php
+++ resources/views/ordinary/tokyo.blade.php
@@ -1,8 +1,8 @@
-@extends('layout.flame')
+@extends('layout.OrdinaryFlame')
 @section('title','Tokyo')
 @section('body')
 <div class="p-3 haikei">
-  
+
   <div class="flex-center position-ref">
     <h1><b>{{$TagPost->title}}</b></h1>
     <ul>
