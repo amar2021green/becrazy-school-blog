0c8b09bfb3e127f69a9c5fe8c3abc04d23c070e6
amar2021green <R17112021s>
2020-02-07 22:40:09 +0900

    軽微なデザイン変更
    ・タイトルを管理画面に変更
    ・アクティブリンクを追加フォームのみに変更

--- resources/views/layout/flame.blade.php
+++ resources/views/layout/flame.blade.php
@@ -37,26 +37,27 @@
 
     <body>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/master/bloglist#">BlogList</a>
+        <a class="navbar-brand" href="/master/bloglist#">管理画面</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="/master/addblog">Add Blog <span class="sr-only">(current)</span></a>
+
+            <li class="nav-item">
+              <a class="nav-link" href="/master/addblog">Add Blog<span class="sr-only">(current)</span></a>
             </li>
 
             <li class="nav-item">
               <a class="nav-link" href="/master/addTag">Add Tag</a>
             </li>
 
-            <li class="nav-item">
+            <li class="nav-item active">
               <a class="nav-link" href="/master/AllCategories">All Category</a>
             </li>
 
-            <li class="nav-item">
+            <li class="nav-item active">
               <a class="nav-link" href="/master/AllTags">All Tag</a>
             </li>
 
