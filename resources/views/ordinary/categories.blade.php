9d4f61595106004e1bcb8e2b358389209814cc05
amar2021green <R17112021s>
2020-02-07 21:48:53 +0900

    Categoryの全表示
    foreachで一覧を回して表示

--- resources/views/ordinary/categories.blade.php
+++ resources/views/ordinary/categories.blade.php
@@ -1,24 +1,31 @@
 @extends('layout.flame')
-@section('title','Category')
+@section('title','Tag')
 @section('body')
-<div class="p-3 mb-2 bg-info text-white">
-
+<div class="p-3 haikei">
+  
   <div class="flex-center position-ref">
     <h1><b>Category</b></h1>
     <ul>
-
+@foreach($AllCategories as $AC)
     <li>
-    {{$category1->type}}
-    {{$category1->name}}
-
+    <a href="/master/AllTags/{{$AC->slug}}">
+    {{$AC->name}}
+  </a>
     </li>
     <br>
 
       @csrf
 
-
+@endforeach
     </ul>
     <br>
+    <a href="http://homestead.test/master/addblog">
+      <button type="button">AddForm</button><br>
+
+        <a href="http://homestead.test/master/addTag">
+          <button type="button">AddTag</button><br>
+
+            <a href="http://homestead.test/master/bloglist">Go Top</a>
 
   </div>
 
