e6d801bbc0afec620698e8895ff38922b8325b9d
amar2021green <R17112021s>
2020-02-07 22:41:53 +0900

    一般View用の軽微な変更
    ・タイトルをWORLD BLOGに変更
    ・記事のタイトルのみ表示させリンク形式に実装

--- resources/views/ordinary/list.blade.php
+++ resources/views/ordinary/list.blade.php
@@ -1,16 +1,17 @@
-@extends('layout.flame')
+@extends('layout.OrdinaryFlame')
 @section('title','BLOG一覧')
 @section('body')
 <div class="p-3 haikei">
-  
+
   <div class="flex-center position-ref">
-    <h1><b>Blog</b></h1>
+    <h1><b>WORLD BLOG</b></h1>
     <ul>
-    @foreach ($all as $blogs)
-    <li>
-    {{$blogs->title}}：
-    {{$blogs->content}}
-    </li>
+      @foreach ($all as $blogs)
+
+        <a class= "text-white" href="/ordinary/list/{{$blogs->slug}}">
+          {{$blogs->title}}
+        </a><br><br>
+
     <br>
 
       @csrf
