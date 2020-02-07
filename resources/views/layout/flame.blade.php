80ac5709fc005822a8f19e2584f572df4fc90faa
amar2021green <R17112021s>
2020-02-07 21:57:33 +0900

    デザイン適用
    ・.haikei（背景画像のデザイン）
    ・nav class（ナビゲーションバー）
    →追加フォーム,一覧画面,ログアウトを配置

--- resources/views/layout/flame.blade.php
+++ resources/views/layout/flame.blade.php
@@ -6,32 +6,21 @@
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-
       <!-- Fonts -->
+
       <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
       <!-- Styles -->
-      <style>
-          html, body {
-              background-color: #fff;
-              color: #636b6f;
-              font-family: 'Nunito', sans-serif;
-              font-weight: 200;
-              height: 100vh;
-              margin: 0;
-          }
 
-          .flex-center {
-              align-items: center;
-              margin: 100px 300px 100px 300px;
-              justify-content: center;
-          }
-
-          .position-ref {
-              position: relative;
-          }
-
+      <style type="text/css">
+      .haikei {
+  background-image: url('/unnamed.jpg');
+  background-position: center center;
+    background-position-x: center;
+    background-position-y: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+}
       </style>
 
 
@@ -47,7 +36,45 @@
     </head>
 
     <body>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/master/bloglist#">BlogList</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/master/addblog">Add Blog <span class="sr-only">(current)</span></a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="/master/addTag">Add Tag</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="/master/AllCategories">All Category</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="/master/AllTags">All Tag</a>
+            </li>
+
+            <li class="nav-item">
+              <form id="logout-form" action="/master/logout" method="POST" >
+              @csrf
+              <input class="btn btn-link" type="submit" value="Logout">
+              </form>
+            </li>
+
+          </ul>
+
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
       @yield("body")
 
     </body>
