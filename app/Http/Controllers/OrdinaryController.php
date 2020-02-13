1ef9c536400f23d303f9ebe7aa6adb40dd0cd861
amar2021green <R17112021s>
2020-02-14 07:27:48 +0900

    一般View用のメソッド名変更

--- app/Http/Controllers/OrdinaryController.php
+++ app/Http/Controllers/OrdinaryController.php
@@ -10,10 +10,10 @@ use Illuminate\Support\Facades\Auth;
 
 class OrdinaryController extends Controller {
 
-   function loggedOut(Request $request)
+   function List(Request $request)
   {
     $all = Post::all();
-      return view('ordinary.list',array('all' => $all));
+      return view('ordinary.List',array('all' => $all));
   }
 
 
@@ -24,28 +24,35 @@ class OrdinaryController extends Controller {
 		return view('ordinary.japan',array('kizi1' => $kizi1));
 	}
 
-  public function TagPost($slug){
-    $TagPost = Post::where('slug',$slug)->first();
-      return view('ordinary.tokyo',array('TagPost' => $TagPost));
+
+  public function AllCategory(){
+    $AllCategories = Taxonomy::where('type','category')->get();
+    return view('ordinary.Categories',array('AllCategories' => $AllCategories));
   }
 
-  public function ShowCategory($slug)
+  public function CategoryPosts($slug)
   {//typeがcategoryでなおかつslugにあてはまるwhere文を入れる
     $category1 = Taxonomy::where([
       ['type','category'],
       ['slug',$slug]])->first();
     //$category1にはTaxonomyモデルのインスタンスが入る
-		return view('ordinary.categories',array('category1' => $category1));
+		return view('ordinary.CategoryPosts',
+    array('posts'=>$category1->posts,'category1' => $category1));
 	}
-
-  public function AllCategory(){
-    $AllCategories = Taxonomy::where('type','category')->get();
-    return view('ordinary.categories',array('AllCategories' => $AllCategories));
-  }
 
   public function AllTag(){
     $AllTags = Taxonomy::where('type','tag')->get();
-    return view('ordinary.tags',array('AllTags' => $AllTags));
+    return view('ordinary.Tags',array('AllTags' => $AllTags));
+  }
+
+  public function TagPost($slug){
+    $Tag_Posts = Taxonomy::where([
+      ['type','tag'],
+      ['slug',$slug]])->first();
+      return view('ordinary.TagPosts',
+      array('posts' => $Tag_Posts->posts,'TagPosts'=>$Tag_Posts));
+      //posts は bladeの変数名($posts)▶タグに紐づく記事一覧
+      //TagPosts は bladeの変数名($TagPosts)▶Taxonomyのtagレコード
   }
 
 
