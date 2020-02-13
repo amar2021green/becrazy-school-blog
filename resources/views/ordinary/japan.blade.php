7beffd416207071cca554a2e7c3618807bfd0763
amar2021green <R17112021s>
2020-02-14 07:30:35 +0900

    Update japan.blade.php

--- resources/views/ordinary/japan.blade.php
+++ resources/views/ordinary/japan.blade.php
@@ -1,8 +1,8 @@
-@extends('layout.flame')
+@extends('layout.OrdinaryFlame')
 @section('title','Japan')
 @section('body')
 <div class="p-3 haikei">
-  
+
   <div class="flex-center position-ref">
     <h1><b>{{$kizi1->title}}</b></h1>
     <ul>
@@ -12,13 +12,13 @@
     {{$kizi1->content}}
 
 
-    <br>
+
 
       @csrf
 
 
     </ul>
-    <br>
+    
 
   </div>
 
